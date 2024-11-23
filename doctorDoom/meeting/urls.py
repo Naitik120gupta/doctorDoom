1fa1e2b98bf92e65637b172949d723dab21d52e7
Naitik <naitik12gupta05@gmail.com>
2024-11-24 00:36:49 +0530

    ctreated login and signup endpts

---
+++ doctorDoom/meeting/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('signup/',views.signup.as_view()),
+    path('login/',views.login.as_view()),
+    path('info/',views.userInfo),
+]
